3d4c0b2aded2eacac33976a6fd6cde5a03fe351b
Adedeji2563 <adedejiadesida1@gmail.com>
2025-03-05 11:52:08 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BotManager from "./components/BotManager";
+
+function App() {
+  return (
+    <div>
+      <h1>Telegram Bot Manager</h1>
+      <BotManager />
+    </div>
+  );
+}
+
+export default App;
